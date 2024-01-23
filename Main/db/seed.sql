059c22f4e022a4c183e44fa63c1a0cc9fe563837
Matt Mullen <mhmullen@icloud.com>
2024-01-22 17:31:50 -0800

    tweaked seeds

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -14,7 +14,7 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Manager', 100000, 1),
+    ('Sales Manager', 125000, 1),
     ('Salesperson', 80000, 1),
     ('Lead Engineer', 150000, 2),
     ('Software Engineer', 120000, 2),
